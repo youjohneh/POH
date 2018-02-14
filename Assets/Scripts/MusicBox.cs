b08646a18870df65e9e5d8f07da1c4b8e1f51c54
MrPotatoSack <ibznakhal@gmail.com>
2018-02-13 19:56:44 -0800

    music cues added

--- Assets/Scripts/MusicBox.cs
+++ Assets/Scripts/MusicBox.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MusicBox : MonoBehaviour {
     public AudioClip clip;
     public AudioSource Source;
+    public float pitch;
+    public float volume;
 	// Use this for initialization
 	void Start () {
         Source = GameObject.FindGameObjectWithTag("MusicBox").GetComponent<AudioSource>();
@@ -15,7 +17,10 @@ public void OnTriggerEnter2D (Collider2D other)
         if(other.tag == "Player")
         {
             Source.clip = clip;
+            Source.pitch = pitch;
+            Source.volume = volume;
             Source.Play();
+
         }
     }
 }
